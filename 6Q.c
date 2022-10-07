64cf04379c1eab2a1e6a6d29daf29df17a1a884b
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-07 22:57:31 +0530

    Create 6Q.c

---
+++ 6Q.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void natural(int);
+int main(){
+    int n;
+    printf("Enter nth term of natural number : ");
+    scanf("%d",&n);
+    natural(2*n);
+    return 0;
+}
+void natural(int n){
+    if(n){
+        if(n%2==0)
+        printf("%d ",n);
+        natural(n-1);
+    }
+    else{
+        return;
+    }
+}
